1ac11f423ccf4fa63ad3b24b50b6ced673b38367
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-05-14 12:37:46 +0300

    Add files via upload

---
+++ Generics/MyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretAgent
+{
+    class MyUniqueList
+    {
+        List<int> list = new List<int>();
+
+        public MyUniqueList()
+        {
+
+        }
+
+        public bool Add(int item)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Remove(int item)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Peek(int index)
+        {
+            return list[index];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return this.list[index];
+            }
+            set
+            {
+                if (list[index] == value)
+                    return;
+                if (list.Contains(value))
+                    return;
+                list[index] = value;
+            }
+        }
+    }
+}
